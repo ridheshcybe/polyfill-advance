efeff9448ef71082824bdb6937e6c9d4fe04124f
cybemachine <80441770+cybemachine@users.noreply.github.com>
2022-03-15 23:24:54 +0545

    minified: web github.dev

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const process = require("process");
+const process=require("process");
 
-Promise.prototype.timeOut = (ms = 1000) =>
-    new Promise((r, j) => setTimeout(r, ms));
-
-Promise.prototype._immediateFn = (fn) => process.nextTick(fn);
+Promise.prototype.timeOut=(e=1e3)=>new Promise((t,o)=>setTimeout(t,e));
+Promise.prototype._immediateFn=(e)=>process.nextTick(e);
 
 Date.prototype.utc = {};
+Object.defineProperties(Date.prototype.utc,{hr:{get:()=>(new Date).getUTCHours()},date:{get:()=>(new Date).getUTCDate()},month:{get:()=>(new Date).getUTCMonth()},min:{get:()=>(new Date).getUTCMinutes()},sec:{get:()=>(new Date).getUTCSeconds()},year:{get:()=>(new Date).getUTCFullYear()},millisec:{get:()=>(new Date).getUTCMilliseconds()}});
 
-Object.defineProperties(Date.prototype.utc, {
-    hr: { get() { return new Date().getUTCHours() } },
-    date: { get() { return new Date().getUTCDate() } },
-    month: { get() { return new Date().getUTCMonth() } },
-    min: { get() { return new Date().getUTCMinutes() } },
-    sec: { get() { return new Date().getUTCSeconds() } },
-    year: { get() { return new Date().getUTCFullYear() } },
-    millisec: { get() { return new Date().getUTCMilliseconds() } }
-})
-
-globalThis.fileDate = (utc = false, native = false) => {
-    const date = new Date();
-    return native ? date.toISOString() : utc ? `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}-${date.getUTCHours()}-${date.getUTCMinutes()}-${date.getUTCSeconds()}-${date.getUTCMilliseconds()}` : `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}-${date.getMilliseconds()}`;
-}
+globalThis.fileDate=((e=!1,t=!1)=>{const o=new Date;return t?o.toISOString():e?`${o.getUTCFullYear()}-${o.getUTCMonth()+1}-${o.getUTCDate()}-${o.getUTCHours()}-${o.getUTCMinutes()}-${o.getUTCSeconds()}-${o.getUTCMilliseconds()}`:`${o.getFullYear()}-${o.getMonth()+1}-${o.getDate()}-${o.getHours()}-${o.getMinutes()}-${o.getSeconds()}-${o.getMilliseconds()}`});
